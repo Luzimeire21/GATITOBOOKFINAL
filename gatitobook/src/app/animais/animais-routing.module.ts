daab2962ea0dbec37abf13e7b758427fbd9cf8e8
Luzimeire21 <104509810+Luzimeire21@users.noreply.github.com>
2022-12-29 08:57:42 -0300

    Initial commit

---
+++ gatitobook/src/app/animais/animais-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
+import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
+import { ListaAnimaisResolver } from './lista-animais/lista-animais.resolver';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ListaAnimaisComponent,
+    resolve: {
+      animais: ListaAnimaisResolver,
+    },
+  },
+  {
+    path: ':animalId',
+    component: DetalheAnimalComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AnimaisRoutingModule {}
---
+++ gatitobook/src/app/animais/animais.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AnimaisRoutingModule } from './animais-routing.module';
+import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
+import { AnimalComponent } from './animal/animal.component';
+import { CartaoModule } from '../componentes/cartao/cartao.module';
+import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
+import { ComentariosComponent } from './detalhe-animal/comentarios/comentarios.component';
+import { MensagemModule } from '../componentes/mensagem/mensagem.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    ListaAnimaisComponent,
+    AnimalComponent,
+    GradeFotosAnimaisComponent,
+    DetalheAnimalComponent,
+    ComentariosComponent,
+  ],
+  imports: [CommonModule, AnimaisRoutingModule, CartaoModule, SharedModule],
+})
+export class AnimaisModule {}
